2af0f6f00e24ccd36b20451c8886497310b45256
schoolnetwork <144613531+schoolnetwork@users.noreply.github.com>
2024-11-20 18:40:14 -0600

    Create uv.config.js

---
+++ public/uv/uv.config.js
@@ -0,0 +1,12 @@
+/*global Ultraviolet*/
+self.__uv$config = {
+  prefix: "/~/uv/",
+  bare: "/w/",
+  encodeUrl: Ultraviolet.codec.plain.encode,
+  decodeUrl: Ultraviolet.codec.plain.decode,
+  handler: "/uv/uv.handler.js",
+  client: "/uv/uv.client.js",
+  bundle: "/uv/uv.bundle.js",
+  config: "/uv/uv.config.js",
+  sw: "/uv/uv.sw.js",
+};
